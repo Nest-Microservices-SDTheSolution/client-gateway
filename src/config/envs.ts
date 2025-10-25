756ffbb877b4c2c266a1be09135d4c13f13c4c17
Gustavo Delgado <gustavo.a.delgado.o@gmail.om>
2025-10-25 15:00:17 -0400

    cambios para gateway de config

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,37 +1,30 @@
 import 'dotenv/config';
 import * as joi from 'joi';
 
-
 interface EnvVars {
-    PORT: number;
-    PRODUCTS_MICROSERVICES_HOST: string;
-    PRODUCTS_MICROSERVICES_PORT: number;
-    ORDERS_MICROSERVICES_HOST: string;
-    ORDERS_MICROSERVICES_PORT: number;
+  PORT: number;
+  NATS_SERVERS: string[];
 }
 
-
-const envsSchema = joi.object({
+const envsSchema = joi
+  .object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
-    ORDERS_MICROSERVICES_HOST: joi.string().required(),
-    ORDERS_MICROSERVICES_PORT: joi.number().required(),
-})
-.unknown(true);
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true);
 
-const {error, value} = envsSchema.validate( process.env );
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
-    throw new Error(`config validation error: ${error.message}`);
+  throw new Error(`config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
 
 export const envs = {
-    port: envVars.PORT,
-    productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICES_HOST,
-    productsMicroservicePort: envVars.PRODUCTS_MICROSERVICES_PORT,
-    ordersMicroserviceHost: envVars.ORDERS_MICROSERVICES_HOST,
-    ordersMicroservicePort: envVars.ORDERS_MICROSERVICES_PORT,
-}
+  port: envVars.PORT,
+  natsServers: envVars.NATS_SERVERS,
+};
